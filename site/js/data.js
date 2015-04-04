f115f9962b89614bcdc62f5b96bd30822262d50d
hunter- <hunter.wapman@gmail.com>
2015-04-04 12:48:14 -0400

    commiting prior to attempting to merge

--- site/js/data.js
+++ site/js/data.js
@@ -1,8 +1,8 @@
 data = [
-	{'provider': "Tasty's Gourmet", 'food':'Hamburger','address':'300 Boston Avenue', 'when': "1:45 PM"},
-	{'provider': "Danish Pastry House", 'food':'Waffle','address':'322 Boston Avenue', 'when': "2:15 PM"},
-	{'provider': "Liza's", 'food':'Pizza','address':'280 Boston Avenue', 'when': "4:30 PM"},
-	{'provider': "Masala", 'food':'Chicken Masala','address':'437 Powerderhouse Boulevard', 'when': "5:00 PM"}
+	{"provider": "Tasty's Gourmet", "food":"Hamburger","address":"300 Boston Avenue", "when": "1:45 PM"},
+	{"provider": "Danish Pastry House", 'food':'Waffle','address':'322 Boston Avenue', 'when': "2:15 PM"},
+	{"provider": "Liza's", 'food':'Pizza','address':'280 Boston Avenue', 'when': "4:30 PM"},
+	{"provider": "Masala", 'food':'Chicken Masala','address':'437 Powerderhouse Boulevard', 'when': "5:00 PM"}
 ];
 
 // // generic form of an offer
