893f447f95a31c5dc2a3b6d291ab58c60d9b22a2
barrymaswan <barrymaswan@gmail.com>
2015-04-24 16:12:55 -0400

    Made major progress byatch

---
+++ js/index.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+	$("#submit_offer_form").hide();
+	$("#claimed_offers").hide();
+	$("#restaurant_div").hide();
+
+
+
+	$("#submit_offer_button").click(function(){
+			$("#map-canvas").hide();
+			$("#submit_offer_form").show();
+			$("#customer_div").hide();
+			$("#restaurant_div").show();
+	});
+
+	$("#show_me_food_button").click(function(){
+			$("#map-canvas").show();
+			$("#submit_offer_form").hide();
+			$("#restaurant_div").hide();
+			$("#customer_div").show();
+	});
+
+	$("#whats_available_button").click(function(){
+			$("#claimed_offers").hide();
+			$("#available_items").show();
+	});
+
+	$("#claimed_offers_button").click(function(){
+			$("#available_items").hide();
+			$("#claimed_offers").show();
+	});
+
+
+});
